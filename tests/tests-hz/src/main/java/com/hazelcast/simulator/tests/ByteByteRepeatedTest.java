a0fd2cbc56c227be3c0bfdb187d059dc5b9e45a9
tezc <ozan.vx@gmail.com>
2019-07-31 00:48:55 +0300

    ByteByteRecurrentTest

--- tests/tests-hz/src/main/java/com/hazelcast/simulator/tests/ByteByteRepeatedTest.java
+++ tests/tests-hz/src/main/java/com/hazelcast/simulator/tests/ByteByteRepeatedTest.java
@@ -1,44 +1,46 @@
-package com.hazelcast.simulator.redisson;
+package com.hazelcast.simulator.tests;
 
+
+import com.hazelcast.core.IMap;
+import com.hazelcast.monitor.NearCacheStats;
+import com.hazelcast.simulator.hz.HazelcastTest;
 import com.hazelcast.simulator.test.BaseThreadState;
 import com.hazelcast.simulator.test.annotations.*;
-import org.redisson.api.LocalCachedMapOptions;
-import org.redisson.api.RMap;
+import com.hazelcast.simulator.worker.loadsupport.Streamer;
+import com.hazelcast.simulator.worker.loadsupport.StreamerFactory;
 
 import java.util.Random;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import static com.hazelcast.simulator.utils.GeneratorUtils.generateByteArray;
 
-public class ByteByteRecurrentTest extends RedissonTest {
+public class ByteByteRepeatedTest extends HazelcastTest {
 
     // properties
     public int keyCount = 1000;
     public int keyLength = 10;
     public int valueCount = 1000;
     public int valueSize = 1000;
-    public int cacheSize = 0;
-    public int recurrentKeyCount;
+    public int repeatedKeyCount;
     public int threadCount = 10;
 
 
     private int perThreadKey;
-    private int perThreadRecurrentKey;
+    private int perThreadRepeatedKey;
     private byte[][] keys;
     private byte[][] values;
 
-    private RMap<byte[], byte[]>[] maps = new RMap[5];
+    private IMap<byte[], byte[]>[] maps = new IMap[5];
     private String[] mapNames = {"map", "map33", "map3", "map12", "map22"};
     private final AtomicInteger id = new AtomicInteger();
-
 
     @Setup
     public void setUp() {
         perThreadKey = keyCount / threadCount;
         System.out.println("perThreadKey " + perThreadKey);
 
-        perThreadRecurrentKey = recurrentKeyCount / threadCount;
-        System.out.println("perThreadRecurrentKey " + perThreadRecurrentKey);
+        perThreadRepeatedKey = repeatedKeyCount / threadCount;
+        System.out.println("perThreadRepeatedKey " + perThreadRepeatedKey);
 
         Random random = new Random();
         keys = new byte[keyCount / maps.length][];
@@ -46,23 +48,10 @@ public class ByteByteRecurrentTest extends RedissonTest {
             keys[i] = generateByteArray(random, keyLength);
         }
 
-        LocalCachedMapOptions options = LocalCachedMapOptions.defaults()
-                .cacheSize(cacheSize)
-                .evictionPolicy(LocalCachedMapOptions.EvictionPolicy.LRU);
-
         for (int i = 0; i < maps.length; i++) {
             String mapName = mapNames[i];
-            if (cacheSize > 0) {
-                maps[i] = client.getLocalCachedMap(mapName, options);
-                System.out.println(" Map name : " + mapName +
-                        " with near cache size  : " + cacheSize +
-                        ", map size :" + this.maps[i].size());
-            } else {
-                maps[i] = client.getMap(mapName);
-                System.out.println(" Map name : " + mapName + "," +
-                        " Regular map no cache" +
-                        " map size " + this.maps[i].size());
-            }
+            maps[i] = targetInstance.getMap(mapName);
+            System.out.println("Map name : " + mapName + ", map size " + maps[i].size());
         }
     }
 
@@ -74,19 +63,23 @@ public class ByteByteRecurrentTest extends RedissonTest {
             values[i] = generateByteArray(random, valueSize);
         }
 
-        for (byte[] key : keys) {
-            for (RMap<byte[], byte[]>map : maps) {
-                map.fastPut(key, values[random.nextInt(values.length)]);
+        for (IMap<byte[], byte[]> map : maps) {
+            Streamer<byte[], byte[]> streamer = StreamerFactory.getInstance(map);
+            for (byte[] key : keys) {
+                streamer.pushEntry(key, values[random.nextInt(values.length)]);
             }
+            streamer.await();
         }
     }
 
     @Prepare (global = true)
     public void prepareGlobal() {
-        for (RMap map : maps) {
-            System.out.println(" Map name : " + map.getName() +
-                    " with near cache size  : " + cacheSize +
-                    ", map size :" + map.size());
+        for (IMap<byte[], byte[]> map : maps) {
+            System.out.println("Total map size " + map.size());
+        }
+
+        for (IMap<byte[], byte[]> map : maps) {
+            System.out.println("Total map size " + map.size());
         }
     }
 
@@ -97,17 +90,17 @@ public class ByteByteRecurrentTest extends RedissonTest {
         state.base = (state.id / maps.length) * perThreadKey;
         state.currentBase = state.base;
 
-        System.out.println("Thread " + state.map.getName() + ", inverval [" + (state.base + "," + state.base + perThreadKey) + "]");
+        System.out.println("Thread " + state.map.getName() + ", inverval [" + state.base + "," + (state.base + perThreadKey) + "]");
     }
 
     @TimeStep(prob = 0)
-    public byte[] putRecurrent(ThreadState state) {
-        return state.map.put(state.randomRecurrentKey(), state.randomValue());
+    public byte[] putRepeated(ThreadState state) {
+        return state.map.put(state.randomRepeatedKey(), state.randomValue());
     }
 
     @TimeStep(prob = 0)
-    public byte[] getRecurrent(ThreadState state) {
-        return state.map.get(state.randomRecurrentKey());
+    public byte[] getRepeated(ThreadState state) {
+        return state.map.get(state.randomRepeatedKey());
     }
 
     @TimeStep(prob = 0)
@@ -121,7 +114,7 @@ public class ByteByteRecurrentTest extends RedissonTest {
     }
 
     public class ThreadState extends BaseThreadState {
-        private RMap<byte[], byte[]> map;
+        private IMap<byte[], byte[]> map;
         private int id;
         private int base;
         private int currentBase;
@@ -129,17 +122,17 @@ public class ByteByteRecurrentTest extends RedissonTest {
         private int count;
 
 
-        private int randomRecurrent() {
-            if (count++ == perThreadRecurrentKey * 100) {
+        private int randomRepeated() {
+            if (count++ == perThreadRepeatedKey * 100) {
                 count = 0;
-                currentBase = ((iteration++ * perThreadRecurrentKey) % perThreadKey) + base;
+                currentBase = ((iteration++ * perThreadRepeatedKey) % perThreadKey) + base;
             }
 
-            return ((count) % perThreadRecurrentKey) + currentBase;
+            return ((count) % perThreadRepeatedKey) + currentBase;
         }
 
-        private byte[] randomRecurrentKey() {
-            return keys[randomRecurrent()];
+        private byte[] randomRepeatedKey() {
+            return keys[randomRepeated()];
         }
 
         private byte[] randomKey() {
@@ -150,16 +143,27 @@ public class ByteByteRecurrentTest extends RedissonTest {
             return values[randomInt(values.length)];
         }
 
-        private RMap<byte[], byte[]> randomMap() {
+        private IMap<byte[], byte[]> randomMap() {
             return maps[randomInt(maps.length)];
+        }
+    }
+
+    @Verify
+    public void printStats() {
+        for (IMap map : maps) {
+            NearCacheStats stats = map.getLocalMapStats().getNearCacheStats();
+            if (stats != null) {
+                System.out.println("Hits : " + stats.getHits() +
+                        "Misses :  " + stats.getMisses() +
+                        "Hit ratio : " + stats.getRatio());
+            }
         }
     }
 
     @Teardown (global = true)
     public void tearDown() {
-        for (RMap map : maps) {
-            map.deleteAsync();
+        for (IMap map : maps) {
+            map.destroy();
         }
     }
-
 }
--- tests/tests-redisson/src/main/java/com/hazelcast/simulator/redisson/ByteByteRepeatedTest.java
+++ tests/tests-redisson/src/main/java/com/hazelcast/simulator/redisson/ByteByteRepeatedTest.java
@@ -1,46 +1,44 @@
-package com.hazelcast.simulator.tests;
+package com.hazelcast.simulator.redisson;
 
-
-import com.hazelcast.core.IMap;
-import com.hazelcast.monitor.NearCacheStats;
-import com.hazelcast.simulator.hz.HazelcastTest;
 import com.hazelcast.simulator.test.BaseThreadState;
 import com.hazelcast.simulator.test.annotations.*;
-import com.hazelcast.simulator.worker.loadsupport.Streamer;
-import com.hazelcast.simulator.worker.loadsupport.StreamerFactory;
+import org.redisson.api.LocalCachedMapOptions;
+import org.redisson.api.RMap;
 
 import java.util.Random;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import static com.hazelcast.simulator.utils.GeneratorUtils.generateByteArray;
 
-public class ByteByteRecurrentTest extends HazelcastTest {
+public class ByteByteRepeatedTest extends RedissonTest {
 
     // properties
     public int keyCount = 1000;
     public int keyLength = 10;
     public int valueCount = 1000;
     public int valueSize = 1000;
-    public int recurrentKeyCount;
+    public int cacheSize = 0;
+    public int repeatedKeyCount;
     public int threadCount = 10;
 
 
     private int perThreadKey;
-    private int perThreadRecurrentKey;
+    private int perThreadRepeatedKey;
     private byte[][] keys;
     private byte[][] values;
 
-    private IMap<byte[], byte[]>[] maps = new IMap[5];
+    private RMap<byte[], byte[]>[] maps = new RMap[5];
     private String[] mapNames = {"map", "map33", "map3", "map12", "map22"};
     private final AtomicInteger id = new AtomicInteger();
+
 
     @Setup
     public void setUp() {
         perThreadKey = keyCount / threadCount;
         System.out.println("perThreadKey " + perThreadKey);
 
-        perThreadRecurrentKey = recurrentKeyCount / threadCount;
-        System.out.println("perThreadRecurrentKey " + perThreadRecurrentKey);
+        perThreadRepeatedKey = repeatedKeyCount / threadCount;
+        System.out.println("perThreadRepeatedKey " + perThreadRepeatedKey);
 
         Random random = new Random();
         keys = new byte[keyCount / maps.length][];
@@ -48,10 +46,23 @@ public class ByteByteRecurrentTest extends HazelcastTest {
             keys[i] = generateByteArray(random, keyLength);
         }
 
+        LocalCachedMapOptions options = LocalCachedMapOptions.defaults()
+                .cacheSize(cacheSize)
+                .evictionPolicy(LocalCachedMapOptions.EvictionPolicy.LRU);
+
         for (int i = 0; i < maps.length; i++) {
             String mapName = mapNames[i];
-            maps[i] = targetInstance.getMap(mapName);
-            System.out.println("Map name : " + mapName + ", map size " + maps[i].size());
+            if (cacheSize > 0) {
+                maps[i] = client.getLocalCachedMap(mapName, options);
+                System.out.println(" Map name : " + mapName +
+                        " with near cache size  : " + cacheSize +
+                        ", map size :" + this.maps[i].size());
+            } else {
+                maps[i] = client.getMap(mapName);
+                System.out.println(" Map name : " + mapName + "," +
+                        " Regular map no cache" +
+                        " map size " + this.maps[i].size());
+            }
         }
     }
 
@@ -63,19 +74,19 @@ public class ByteByteRecurrentTest extends HazelcastTest {
             values[i] = generateByteArray(random, valueSize);
         }
 
-        for (IMap<byte[], byte[]> map : maps) {
-            Streamer<byte[], byte[]> streamer = StreamerFactory.getInstance(map);
-            for (byte[] key : keys) {
-                streamer.pushEntry(key, values[random.nextInt(values.length)]);
+        for (byte[] key : keys) {
+            for (RMap<byte[], byte[]>map : maps) {
+                map.fastPut(key, values[random.nextInt(values.length)]);
             }
-            streamer.await();
         }
     }
 
     @Prepare (global = true)
     public void prepareGlobal() {
-        for (IMap<byte[], byte[]> map : maps) {
-            System.out.println("Total map size " + map.size());
+        for (RMap map : maps) {
+            System.out.println(" Map name : " + map.getName() +
+                    " with near cache size  : " + cacheSize +
+                    ", map size :" + map.size());
         }
     }
 
@@ -86,17 +97,17 @@ public class ByteByteRecurrentTest extends HazelcastTest {
         state.base = (state.id / maps.length) * perThreadKey;
         state.currentBase = state.base;
 
-        System.out.println("Thread " + state.map.getName() + ", inverval [" + state.base + "," + (state.base + perThreadKey) + "]");
+        System.out.println("Thread " + state.map.getName() + ", inverval [" + (state.base + "," + state.base + perThreadKey) + "]");
     }
 
     @TimeStep(prob = 0)
-    public byte[] putRecurrent(ThreadState state) {
-        return state.map.put(state.randomRecurrentKey(), state.randomValue());
+    public byte[] putRepeated(ThreadState state) {
+        return state.map.put(state.randomRepeatedKey(), state.randomValue());
     }
 
     @TimeStep(prob = 0)
-    public byte[] getRecurrent(ThreadState state) {
-        return state.map.get(state.randomRecurrentKey());
+    public byte[] getRepeated(ThreadState state) {
+        return state.map.get(state.randomRepeatedKey());
     }
 
     @TimeStep(prob = 0)
@@ -110,7 +121,7 @@ public class ByteByteRecurrentTest extends HazelcastTest {
     }
 
     public class ThreadState extends BaseThreadState {
-        private IMap<byte[], byte[]> map;
+        private RMap<byte[], byte[]> map;
         private int id;
         private int base;
         private int currentBase;
@@ -118,17 +129,17 @@ public class ByteByteRecurrentTest extends HazelcastTest {
         private int count;
 
 
-        private int randomRecurrent() {
-            if (count++ == perThreadRecurrentKey * 100) {
+        private int randomRepeated() {
+            if (count++ == perThreadRepeatedKey * 100) {
                 count = 0;
-                currentBase = ((iteration++ * perThreadRecurrentKey) % perThreadKey) + base;
+                currentBase = ((iteration++ * perThreadRepeatedKey) % perThreadKey) + base;
             }
 
-            return ((count) % perThreadRecurrentKey) + currentBase;
+            return ((count) % perThreadRepeatedKey) + currentBase;
         }
 
-        private byte[] randomRecurrentKey() {
-            return keys[randomRecurrent()];
+        private byte[] randomRepeatedKey() {
+            return keys[randomRepeated()];
         }
 
         private byte[] randomKey() {
@@ -139,25 +150,16 @@ public class ByteByteRecurrentTest extends HazelcastTest {
             return values[randomInt(values.length)];
         }
 
-        private IMap<byte[], byte[]> randomMap() {
+        private RMap<byte[], byte[]> randomMap() {
             return maps[randomInt(maps.length)];
-        }
-    }
-
-    @Verify
-    public void printStats() {
-        for (IMap map : maps) {
-            NearCacheStats stats = map.getLocalMapStats().getNearCacheStats();
-            if (stats != null) {
-                System.out.println("Hit ratio : " + stats.getRatio());
-            }
         }
     }
 
     @Teardown (global = true)
     public void tearDown() {
-        for (IMap map : maps) {
-            map.destroy();
+        for (RMap map : maps) {
+            map.deleteAsync();
         }
     }
+
 }
